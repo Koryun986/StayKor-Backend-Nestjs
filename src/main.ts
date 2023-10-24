9961f9d45883d36c8da85c3b83fcb85e84b3a563
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-10-24 18:48:32 +0400

    add config file and envirement file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+  app.enableCors({
+    credentials: true,
+    origin: configService.get<string>("frontendUrl"),
+  });
+  console.log("Server Start: " + configService.get<number>("port"));
+  await app.listen(configService.get<number>("port"));
 }
 bootstrap();
