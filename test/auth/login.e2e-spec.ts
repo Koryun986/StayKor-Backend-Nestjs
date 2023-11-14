a0205cbdeb7795141eb40a027ccaed0f554b6f1d
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-11-14 18:49:46 +0400

    login test: should throw exception when access token invalid

--- test/auth/login.e2e-spec.ts
+++ test/auth/login.e2e-spec.ts
@@ -5,10 +5,14 @@ import { AuthModule } from "src/auth/auth.module";
 import { AppModule } from "src/app.module";
 import { correctBody } from "./utils";
 import { AuthResponse } from "src/auth/types/auth-response.type";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 describe("/auth/login (POST)", () => {
   let app: INestApplication;
   const url = "/auth/login";
+  const jwtService = new JwtService();
+  const configService = new ConfigService();
   let testRequest: request.Test | undefined;
 
   beforeAll(async () => {
@@ -18,8 +22,12 @@ describe("/auth/login (POST)", () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    const accessToken = await jwtService.signAsync(correctBody, {
+      secret: configService.get<string>("jwt.secret"),
+    });
     testRequest = request(app.getHttpServer())
       .post(url)
+      .set({ Authorization: accessToken })
       .send({ email: correctBody.email, password: correctBody.password });
   });
 
@@ -36,5 +44,13 @@ describe("/auth/login (POST)", () => {
         expect(body.password).not.toBeDefined();
       },
     );
+  });
+
+  it("expect bad request error when access token is expired", async () => {
+    return await request(app.getHttpServer())
+      .post(url)
+      .set({ Authorization: "wrong.access.token" })
+      .send(correctBody)
+      .expect(400);
   });
 });
