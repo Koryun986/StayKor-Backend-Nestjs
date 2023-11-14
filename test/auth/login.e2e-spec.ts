f121a1ade3ecf44157177fbf98ec149077a49790
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-11-14 18:31:13 +0400

    login test : expect correct resposne on correct body

--- test/auth/login.e2e-spec.ts
+++ test/auth/login.e2e-spec.ts
@@ -3,6 +3,8 @@ import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
 import { AuthModule } from "src/auth/auth.module";
 import { AppModule } from "src/app.module";
+import { correctBody } from "./utils";
+import { AuthResponse } from "src/auth/types/auth-response.type";
 
 describe("/auth/login (POST)", () => {
   let app: INestApplication;
@@ -16,5 +18,23 @@ describe("/auth/login (POST)", () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    testRequest = request(app.getHttpServer())
+      .post(url)
+      .send({ email: correctBody.email, password: correctBody.password });
+  });
+
+  it("expect 201 on correct body", () => {
+    return testRequest.expect(201);
+  });
+
+  it("expect correct response on correct body", () => {
+    return testRequest.then(
+      ({ body }: { body: AuthResponse & { password: string } }) => {
+        expect(body.email).toBe(correctBody.email);
+        expect(body.name).toBe(correctBody.name);
+        expect(body.access_token.length).toBeGreaterThan(0);
+        expect(body.password).not.toBeDefined();
+      },
+    );
   });
 });
