ab7428590ef7c7410704109c2c4b21238061e1e4
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-11-21 19:26:41 +0400

    add refresh test to index.e2e-spec.ts

--- test/auth/index.e2e-spec.ts
+++ test/auth/index.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { loginTest } from "./login.spec";
+import { refreshTest } from "./refresh.spec";
 import { registrationTest } from "./registration.spec";
 
 describe("Auth test", () => {
   registrationTest();
   loginTest();
+  refreshTest();
 });
