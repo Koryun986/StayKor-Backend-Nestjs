7eac3a0c6b0e8f98ee865556eac0683b4d51f0d9
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-12-03 20:19:44 +0400

    create lodging test which expect 201 when everything alright

--- test/lodgings/create-lodgings.e2e-spec.ts
+++ test/lodgings/create-lodgings.e2e-spec.ts
@@ -1,11 +1,20 @@
 import { INestApplication } from "@nestjs/common";
 import { TestingModule, Test } from "@nestjs/testing";
 import * as request from "supertest";
+import { correctAccessToken } from "test/auth/registration.spec";
 
 describe("/lodgings/create (POST)", () => {
   let app: INestApplication;
   const url = "/lodgings/create";
   let testRequest: request.Test | undefined;
+  const correctLodgingBody = {
+    price: "200",
+    address: {
+      country: "USA",
+      city: "LA",
+      address: "somewhere house 2",
+    },
+  };
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -16,5 +25,13 @@ describe("/lodgings/create (POST)", () => {
     await app.init();
 
     testRequest = request(app.getHttpServer()).post(url);
+  });
+
+  it("expect 201 status when everything is correct", () => {
+    return testRequest
+      .set("Authorization", `Bearer ${correctAccessToken}`)
+      .attach("lodging photo", "../../../assets/lodging_photo.jpg")
+      .send(correctLodgingBody)
+      .expect(201);
   });
 });
