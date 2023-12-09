3be8c0311d5b4d4bbf208d37682c3b792232ef5b
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-12-09 13:32:17 +0400

    create lodging test upgrade

--- test/lodgings/create-lodgings.e2e-spec.ts
+++ test/lodgings/create-lodgings.e2e-spec.ts
@@ -1,6 +1,8 @@
+import { createReadStream } from "fs";
 import { INestApplication } from "@nestjs/common";
 import { TestingModule, Test } from "@nestjs/testing";
 import * as request from "supertest";
+import * as streamToBlob from "stream-to-blob";
 import { correctAccessToken } from "test/auth/registration.spec";
 
 describe("/lodgings/create (POST)", () => {
@@ -14,6 +16,8 @@ describe("/lodgings/create (POST)", () => {
       city: "LA",
       address: "somewhere house 2",
     },
+    description:
+      "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim",
   };
 
   beforeAll(async () => {
@@ -28,10 +32,24 @@ describe("/lodgings/create (POST)", () => {
   });
 
   it("expect 201 status when everything is correct", () => {
+    const formData = new FormData();
+    const readStream = createReadStream("./../../../assets/lodging_photo.jpg");
+    streamToBlob(readStream, (err: Error, blob: Blob) => {
+      if (err)
+        return new Error(
+          "Something went wrong while converting stream to blob",
+        );
+
+      formData.append("file[]", blob);
+    });
+    formData.append("price", correctLodgingBody.price);
+    formData.append("address.country", correctLodgingBody.address.country);
+    formData.append("address.city", correctLodgingBody.address.city);
+    formData.append("address.address", correctLodgingBody.address.address);
+    formData.append("description", correctLodgingBody.description);
     return testRequest
       .set("Authorization", `Bearer ${correctAccessToken}`)
-      .attach("lodging photo", "../../../assets/lodging_photo.jpg")
-      .send(correctLodgingBody)
+      .send(formData)
       .expect(201);
   });
 });
