c8beb631c692517ef6c599ad88fd9e18fed08cc1
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-12-12 17:35:55 +0400

    feat(lodging controller): new post route for creating lodging

--- src/lodgings/controller/lodgings.controller.ts
+++ src/lodgings/controller/lodgings.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post, UseInterceptors } from "@nestjs/common";
+import { FilesInterceptor } from "@nestjs/platform-express";
 import { LodgingsService } from "../service/lodgings.service";
 
 @Controller("lodgings")
 export class LodgingsController {
   constructor(private lodgingsService: LodgingsService) {}
+
+  @Post("/create")
+  @UseInterceptors(FilesInterceptor("files"))
+  async createLodging() {}
 }
