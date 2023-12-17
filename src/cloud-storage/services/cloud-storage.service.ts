7ba336b05bb97af895381ad2151437e38dcd05ab
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-12-17 13:18:03 +0400

    feat(cloud-storage): add upload function

--- src/cloud-storage/services/cloud-storage.service.ts
+++ src/cloud-storage/services/cloud-storage.service.ts
@@ -4,8 +4,29 @@ import { FirebaseCloudStorageService } from "src/firebase/firebase.service";
 @Injectable()
 export class CloudStorageService {
   private readonly app = this.firebaseCloudStorageService.app;
+  private readonly storage = this.app.storage().bucket();
 
   constructor(
     private readonly firebaseCloudStorageService: FirebaseCloudStorageService,
   ) {}
+
+  async uploadFile(
+    files: Array<Express.Multer.File>,
+    userId: number,
+    lodgingId: number,
+  ) {
+    try {
+      const folderName = `user_${userId}/lodging_${lodgingId}`;
+      files.forEach(async ({ buffer, originalname, mimetype }) => {
+        const filePath = `${folderName}/${Date.now()}_${originalname}`;
+        await this.storage.file(filePath).save(buffer, {
+          metadata: {
+            contentType: mimetype,
+          },
+        });
+      });
+    } catch (e) {
+      throw new Error("Can't upload files to Firebase Cloud Storage");
+    }
+  }
 }
