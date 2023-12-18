8bf77a4f3aaef43d8ba36ff9eeaf08b210e42ffb
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-12-18 18:53:52 +0400

    feat(lodging service): inject Lodging Reopos dependency

--- src/lodgings/lodgings.module.ts
+++ src/lodgings/lodgings.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { LodgingsController } from "./controller/lodgings.controller";
 import { LodgingsService } from "./service/lodgings.service";
 import { JwtServiceModule } from "src/jwt-service/jwt-service.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Lodging } from "src/typeorm/entities/lodging.entity";
 
 @Module({
-  imports: [JwtServiceModule],
+  imports: [JwtServiceModule, TypeOrmModule.forFeature([Lodging])],
   controllers: [LodgingsController],
   providers: [LodgingsService],
 })
--- src/lodgings/service/lodgings.service.ts
+++ src/lodgings/service/lodgings.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { CreateLodgingDto } from "../dto/create-lodging.dto";
+import { Repository } from "typeorm";
+import { Lodging } from "src/typeorm/entities/lodging.entity";
+import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
-export class LodgingsService {}
+export class LodgingsService {
+  constructor(
+    @InjectRepository(Lodging) private lodgingRepository: Repository<Lodging>,
+  ) {}
+
+  async createLodging(
+    images: Array<Express.Multer.File>,
+    lodgingDto: CreateLodgingDto,
+  ) {}
+}
