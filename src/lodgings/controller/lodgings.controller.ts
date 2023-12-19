550007bb7ce5feb45bbd9c065a45d72bbe30dc14
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-12-19 18:12:31 +0400

    feat(lodging controller): add dto pipe to create lodging route

--- src/lodgings/controller/lodgings.controller.ts
+++ src/lodgings/controller/lodgings.controller.ts
@@ -6,12 +6,17 @@ import {
   UseInterceptors,
   UseGuards,
   Res,
+  UsePipes,
 } from "@nestjs/common";
 import { FilesInterceptor } from "@nestjs/platform-express";
 import { LodgingsService } from "../service/lodgings.service";
-import { CreateLodgingDto } from "../dto/create-lodging.dto";
+import {
+  CreateLodgingDto,
+  createLodgingSchema,
+} from "../dto/create-lodging.dto";
 import { AuthGuard } from "src/guards/auth/auth.guard";
 import { UserInterceptor } from "src/interceptors/auth/user.interceptor";
+import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
 
 @Controller("lodgings")
 export class LodgingsController {
@@ -21,6 +26,7 @@ export class LodgingsController {
   @UseInterceptors(FilesInterceptor("files"))
   @UseInterceptors(UserInterceptor)
   @UseGuards(AuthGuard)
+  @UsePipes(new ZodValidationPipe(createLodgingSchema))
   async createLodging(
     @UploadedFiles() files: Array<Express.Multer.File>,
     @Body() lodgingDto: CreateLodgingDto,
