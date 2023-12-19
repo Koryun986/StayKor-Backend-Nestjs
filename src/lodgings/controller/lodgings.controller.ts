fd5b80d5f518228789c010d6b359ee0d2ae88621
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-12-19 18:19:46 +0400

    feat(lodging controller): implement create lodging route

--- src/lodgings/controller/lodgings.controller.ts
+++ src/lodgings/controller/lodgings.controller.ts
@@ -1,22 +1,23 @@
 import {
+  Body,
   Controller,
   Post,
-  Body,
+  Req,
   UploadedFiles,
-  UseInterceptors,
   UseGuards,
-  Res,
+  UseInterceptors,
   UsePipes,
 } from "@nestjs/common";
 import { FilesInterceptor } from "@nestjs/platform-express";
-import { LodgingsService } from "../service/lodgings.service";
+import { AuthGuard } from "src/guards/auth/auth.guard";
+import { UserInterceptor } from "src/interceptors/auth/user.interceptor";
+import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
+import { User } from "src/typeorm/entities/user.entity";
 import {
   CreateLodgingDto,
   createLodgingSchema,
 } from "../dto/create-lodging.dto";
-import { AuthGuard } from "src/guards/auth/auth.guard";
-import { UserInterceptor } from "src/interceptors/auth/user.interceptor";
-import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
+import { LodgingsService } from "../service/lodgings.service";
 
 @Controller("lodgings")
 export class LodgingsController {
@@ -30,6 +31,14 @@ export class LodgingsController {
   async createLodging(
     @UploadedFiles() files: Array<Express.Multer.File>,
     @Body() lodgingDto: CreateLodgingDto,
-    @Res() resposne: Response,
-  ) {}
+    @Req() request: Request,
+  ) {
+    const user: User = (request as any).user;
+    const data = await this.lodgingsService.createLodging(
+      files,
+      lodgingDto,
+      user.id,
+    );
+    return data;
+  }
 }
