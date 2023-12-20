896a9791fef5b954f6674bc495d232a31670e7b2
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-12-20 10:13:01 +0400

    chore(lodging module): remove unused dependencies

--- src/lodgings/lodgings.module.ts
+++ src/lodgings/lodgings.module.ts
@@ -1,9 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { APP_INTERCEPTOR } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CloudStorageService } from "src/cloud-storage/services/cloud-storage.service";
-import { UserInterceptor } from "src/interceptors/auth/user.interceptor";
 import { JwtServiceModule } from "src/jwt-service/jwt-service.module";
 import { Address } from "src/typeorm/entities/address.entity";
 import { LodgingImages } from "src/typeorm/entities/lodging-images.entity";
