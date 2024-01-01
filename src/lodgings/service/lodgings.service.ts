62456112924cbb3621b0e5e2d247e86c87cf7d76
Koryun <79407889+Koryun986@users.noreply.github.com>
2024-01-01 20:31:02 +0400

    chore(create lodging service): rewrite all code with transactions

--- src/lodgings/service/lodgings.service.ts
+++ src/lodgings/service/lodgings.service.ts
@@ -5,7 +5,7 @@ import { LodgingImagesRepository } from "src/repositories/lodging/lodging-images
 import { Address } from "src/typeorm/entities/address.entity";
 import { LodgingImages } from "src/typeorm/entities/lodging-images.entity";
 import { Lodging } from "src/typeorm/entities/lodging.entity";
-import { Repository } from "typeorm";
+import { DataSource, Repository } from "typeorm";
 import { AddressDto } from "../dto/address.dto";
 import { CreateLodgingDto } from "../dto/create-lodging.dto";
 
@@ -16,6 +16,7 @@ export class LodgingsService {
     @InjectRepository(Address) private addressRepository: Repository<Address>,
     @InjectRepository(LodgingImages)
     private lodgingImagesRepository: LodgingImagesRepository,
+    private readonly dataSource: DataSource,
     private readonly cloudStorageService: CloudStorageService,
   ) {}
 
@@ -24,31 +25,47 @@ export class LodgingsService {
     lodgingDto: CreateLodgingDto,
     userId: number,
   ) {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
     try {
-      const lodging: Lodging = await this.createAndSaveLodging(
+      const address = this.createAddress(lodgingDto.address);
+      const savedAddress = await queryRunner.manager.save(address);
+
+      const lodging: Lodging = this.createLodgingEntity(
         lodgingDto,
         userId,
+        savedAddress.id,
       );
+      const savedLodging = await queryRunner.manager.save(lodging);
       const downloadUrls = await this.cloudStorageService.uploadFile(
         images,
         userId,
-        lodging.id,
+        savedLodging.id,
       );
-      await this.saveDownloadUrls(downloadUrls, lodging.id);
+      const lodgingImages = this.createLodgingImages(downloadUrls, lodging.id);
+      await queryRunner.manager.save(lodgingImages);
+
+      await queryRunner.commitTransaction();
+
       return {
         ...lodging,
         downloadUrls: downloadUrls,
       };
     } catch (e) {
-      throw new Error(e);
+      await queryRunner.rollbackTransaction();
+      throw e;
+    } finally {
+      await queryRunner.release();
     }
   }
 
-  async createAndSaveLodging(
+  createLodgingEntity(
     lodgingDto: CreateLodgingDto,
     userId: number,
-  ): Promise<Lodging> {
-    const addressId = await this.createAddressAndGetId(lodgingDto.address);
+    addressId: number,
+  ): Lodging {
     const lodging = this.lodgingRepository.create({
       description: lodgingDto.description,
       price: lodgingDto.price,
@@ -56,27 +73,25 @@ export class LodgingsService {
       userId,
     });
 
-    await this.lodgingRepository.save(lodging);
     return lodging;
   }
 
-  async createAddressAndGetId(addressDto: AddressDto): Promise<number> {
+  createAddress(addressDto: AddressDto) {
     const address: Address = this.addressRepository.create({
       city: addressDto.city,
       country: addressDto.country,
       address: addressDto.address,
     });
-    await this.addressRepository.save(address);
-    return address.id;
+
+    return address;
   }
 
-  async saveDownloadUrls(urls: string[], lodgingId: number) {
+  createLodgingImages(urls: string[], lodgingId: number) {
     const lodgingImages = this.lodgingImagesRepository.createLodgingImages({
       lodgingId,
       downloadUrls: urls,
     });
 
-    await this.lodgingImagesRepository.save(lodgingImages);
     return lodgingImages;
   }
 }
