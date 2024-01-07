954579e4c761d13d4afc8da766d3a12399d78000
Koryun <79407889+Koryun986@users.noreply.github.com>
2024-01-07 11:31:48 +0400

    feat(graphql types): create graphql objecttype over entities

--- src/typeorm/entities/address.entity.ts
+++ src/typeorm/entities/address.entity.ts
@@ -1,18 +1,24 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@ObjectType()
 @Entity()
 export class Address {
+  @Field(() => Int)
   @PrimaryGeneratedColumn({
     type: "bigint",
   })
   id: number;
 
+  @Field()
   @Column()
   country: string;
 
+  @Field()
   @Column()
   city: string;
 
+  @Field()
   @Column()
   address: string;
 }
--- src/typeorm/entities/lodging.entity.ts
+++ src/typeorm/entities/lodging.entity.ts
@@ -7,22 +7,28 @@ import {
 } from "typeorm";
 import { User } from "./user.entity";
 import { Address } from "./address.entity";
+import { Field, Int, ObjectType } from "@nestjs/graphql";
 
+@ObjectType()
 @Entity()
 export class Lodging {
+  @Field(() => Int)
   @PrimaryGeneratedColumn({
     type: "bigint",
   })
   id: number;
 
+  @Field()
   @Column()
   price: string;
 
+  @Field()
   @Column({
     type: "longtext",
   })
   description: string;
 
+  @Field(() => Address)
   @OneToOne(() => Address)
   address: Address;
 
